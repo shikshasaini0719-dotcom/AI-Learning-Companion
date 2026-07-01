14b482142f780bcbf69ec2469fae04d13eae7534
Shiksha Saini <shikshasaini0719@gmail.com>
2026-07-01 10:56:21 +0530

    Working state before modular refactor

--- pages/quiz.py
+++ pages/quiz.py
@@ -1,4 +1,34 @@
 import streamlit as st
+import time
+
+from database import conn, cursor
+from pdf_generator import generate_pdf
+from utils.ai_tutor import show_ai_tutor
+from utils.analytics import (
+    calculate_accuracy,
+    calculate_grade,
+    show_performance_report,
+    show_analytics_dashboard,
+)
+
+def load_leaderboard():
+    data = []
+
+    try:
+        with open("leaderboard.txt", "r") as f:
+            for line in f:
+                parts = line.strip().split(",")
+
+                if len(parts) == 2:
+                    name = parts[0]
+                    score = int(parts[1])
+                    data.append((name, score))
+    except:
+        pass
+
+    data.sort(key=lambda x: x[1], reverse=True)
+    return data
+
 
 st.title("📝 Quiz")
 
@@ -6,8 +36,35 @@ if not st.session_state.started:
     st.warning("⚠️ Please start the quiz from the Home page.")
     st.stop()
 
-st.success("Quiz page connected successfully!")
 
-st.write("Name:", st.session_state.name)
-st.write("Subject:", st.session_state.subject)
-st.write("Questions Loaded:", len(st.session_state.questions))
+    if st.session_state.started and st.session_state.questions:
+
+    q_index = st.session_state.q_index
+    questions = st.session_state.questions
+
+    # Progress
+    if st.session_state.target_questions > 0:
+        progress = min(
+            st.session_state.attempted /
+            st.session_state.target_questions,
+            1.0
+        )
+    else:
+        progress = 0
+
+    st.progress(progress)
+
+    st.info(
+        f"""
+📚 Total Questions Available: {len(st.session_state.questions)}
+
+🎯 Questions Required: {st.session_state.target_questions}
+
+✅ Attempted: {st.session_state.attempted}
+
+⏭️ Skipped: {st.session_state.skipped_count}
+
+🎯 Questions Left To Solve:
+{st.session_state.target_questions - st.session_state.attempted}
+"""
+    )
